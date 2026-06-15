9880c176674225b6a27a4ba2171b3e88d02c7b30
Oria Yosef <oriayo@std.jmc.ac.il>
2026-06-15 16:57:30 +0300

    ggg

--- include/Player.h
+++ include/Player.h
@@ -6,14 +6,14 @@
 class Player //: public StaticObject do an heart tile
 {
 public:
-
+	Player();
     //void draw(sf::RenderWindow& window) const;
     //const sf::Vector2f& getPosition() const;
 
 private:
     //sf::Sprite m_sprite;
     //int m_health;
-	Heart m_heart;
+	Heart* m_heart;
     int m_keys;
     //std::vector<std::unique_ptr<Monster>> m_monsters;
 };
