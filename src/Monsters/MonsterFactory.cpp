275aab1de95ac271c4f4f3f1f5332c8f0c400f51
Yair Krothamer <yairkr@edu.hac.ac.il>
2026-08-26 16:16:21 +0300

    Clear Comments

--- src/Monsters/MonsterFactory.cpp
+++ src/Monsters/MonsterFactory.cpp
@@ -1,9 +1,6 @@
 #include "Monsters/MonsterFactory.h"
 #include "Card.h"
 #include <iostream>
-
-// ��� �-includes ������� ��� ������� ������� �� ������� ����������.
-// �� ���� ������ ����� ���� - �� ����� ����� ����� ���� �� (��� ������ �� ����� ����).
 #include "Monsters/Muffintop.h"
 #include "Monsters/Blue.h"
 #include "Monsters/Barzilla.h"
@@ -24,12 +21,6 @@ std::map<std::string, MonsterFactory::Creator>& MonsterFactory::getRegistry()
 
     return registry;
 }
-//std::map<std::string, MonsterFactory::Creator>& MonsterFactory::getRegistry()
-//{
-//    // static ���� ������� -> ������ ��� ���, ��� ���� ���� ����� ������
-//    static std::map<std::string, Creator> registry;
-//    return registry;
-//}
 
 void MonsterFactory::registerMonster(const std::string& name, Creator creator)
 {
@@ -48,26 +39,7 @@ std::unique_ptr<Monster> MonsterFactory::create(const std::string& monsterName, 
     }
     return it->second(side);
 }
-//std::unique_ptr<Monster> MonsterFactory::create(const std::string& monsterName)
-//{
-//    auto& registry = getRegistry();
-//    auto it = registry.find(monsterName);
-//    if (it == registry.end())
-//        return nullptr; // �� �� ���� - �� ������, ������� nullptr
-//
-//    return it->second();
-//}
 
-//std::vector<std::unique_ptr<Monster>> MonsterFactory::createStandardDeck(PlayerSide side)
-//{
-//    std::vector<std::unique_ptr<Monster>> deck;
-//    deck.push_back(std::make_unique<Muffintop>(side));
-//    deck.push_back(std::make_unique<Blue>(side));
-//    deck.push_back(std::make_unique<Barzilla>(side));
-//    deck.push_back(std::make_unique<Henrietta>(side));
-//    deck.push_back(std::make_unique<Mozzy>(side));
-//    return deck;
-//}
 std::vector<std::unique_ptr<Card>> MonsterFactory::createStandardHand(PlayerSide side)
 {
     std::vector<std::unique_ptr<Card>> hand;
